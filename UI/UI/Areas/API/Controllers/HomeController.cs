7ca31ddec98ffb6ca2fb93a7fcf8fc7084c5c62c
qiuyuhan <1031893464@qq.com>
2018-09-27 19:32:51 +0800

    架构

---
+++ UI/UI/Areas/API/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Areas.API.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: API/Home
+        public ActionResult Index()
+        {
+            return Content("空API");
+        }
+    }
+}
